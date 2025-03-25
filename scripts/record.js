7c8608a9d0d17a19d9237a06f3585efd463f4d1f
antsuarz <uo270543@uniovi.es>
2025-03-25 19:32:17 +0100

    Cambios en el funcionamiento y descarga

---
+++ scripts/record.js
@@ -0,0 +1,53 @@
+let mediaRecorder;
+let recordedChunks = [];
+
+function startRecording() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+        .then(stream => {
+            mediaRecorder = new MediaRecorder(stream);
+
+            mediaRecorder.ondataavailable = event => {
+                if (event.data.size > 0) {
+                    recordedChunks.push(event.data);
+                }
+            };
+
+            mediaRecorder.start();
+        })
+        .catch(error => console.error("Error al acceder a la cámara:", error));
+}
+ let isDownloading = false;
+
+function stopRecordingAndDownload() { 
+
+    if (mediaRecorder && mediaRecorder.state !== "inactive") {
+        mediaRecorder.stop();
+        isDownloading = true;
+
+        mediaRecorder.onstop = () => { 
+ 
+            const url = URL.createObjectURL(new Blob(recordedChunks, { type: "video/webm" }));
+            const a = document.createElement("a");
+            
+            const timestamp = new Date().toISOString().replace(/[:.]/g, "-"); 
+
+            a.href = url;
+            a.download = `video_${timestamp}.webm`;
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+            URL.revokeObjectURL(url);
+ 
+            isDownloading = false;
+        };
+    } else {
+        console.error("Ha habido un error al descargar la grabacion");
+    }
+}
+
+window.addEventListener("beforeunload", function (event) {
+    if (isDownloading) {
+        event.preventDefault();
+    }
+});
+
